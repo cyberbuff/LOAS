d1a2938b0f957c110583b5225da6c864020350c0
Hare Sudhan <code@0x6c.dev>
2025-06-18 01:37:40 -0400

    add validation & compilation scripts

---
+++ docs/src/app/(home)/page.tsx
@@ -0,0 +1,14 @@
+export default function HomePage() {
+	return (
+		<main className="flex flex-1 flex-col justify-center text-center">
+			<h1 className="mb-4 text-2xl font-bold">
+				Living off the Orchard: Apple Script
+			</h1>
+			<p className="text-fd-muted-foreground">
+				<b>L</b>iving off the <b>O</b>rchard: <b>A</b>pple <b>S</b>cript is
+				designed to provide detailed information on various scripts and how they
+				can be used by threat actors for malicious purposes.
+			</p>
+		</main>
+	);
+}
